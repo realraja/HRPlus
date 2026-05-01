c08ae66a1dee6346046c3e39fc0a92147a7acc90
Raja <realllraja@gmail.com>
2026-05-02 01:01:18 +0530

    Initial project setup

---
+++ src/main/java/com/hrms/HRPlus/HrPlusApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.HRPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrms/HRPlus/HrPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.HRPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
